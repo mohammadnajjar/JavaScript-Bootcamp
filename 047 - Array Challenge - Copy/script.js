8054ec7b97ea48c8756c96229743ff8cd9989b06
MGLNaJ <30768546+mohammadnajjar@users.noreply.github.com>
2021-07-23 05:07:29 -0400

    h

---
+++ 047 - Array Challenge - Copy/script.js
@@ -0,0 +1,20 @@
+/*
+  Array Challenge
+  https://elzero.org/javascript-array-methods-challenge/
+*/
+
+let zero = 0;
+
+let counter = 3;
+
+let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
+
+// Write Code Here
+
+console.log(my); // ["Osama", "Elham", "Mazero", "Ahmed"];
+
+console.log(my.slice("????")); // ["Elham", "Mazero"]
+
+console.log(); // "Elzero"
+
+console.log(); // "rO"
